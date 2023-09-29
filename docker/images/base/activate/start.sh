e82e93d10e840f8a9219d2e0baa77ad89c1e771b
jeng-yuantsai <tsaie79@gmail.com>
2023-09-28 20:33:54 -0400

    now JOBNAME is replaced by NODENAME

--- docker/images/base/activate/start.sh
+++ docker/images/base/activate/start.sh
@@ -7,6 +7,6 @@ export APISERVER_CERT_LOCATION="$VK_PATH/client.crt"
 export APISERVER_KEY_LOCATION="$VK_PATH/client.key"
 export KUBELET_PORT="10250"
 
-echo "{\"$JOBNAME\": {\"cpu\": \"0\", \"memory\": \"0Gi\", \"pods\": \"0\"}}" > $HOME/.host-cfg.json
+echo "{\"$NODENAME\": {\"cpu\": \"0\", \"memory\": \"0Gi\", \"pods\": \"0\"}}" > $HOME/.host-cfg.json
 
-"$VK_PATH/virtual-kubelet" --nodename $JOBNAME --provider mock --provider-config $HOME/.host-cfg.json --klog.v 3 > $HOME/$JOBNAME.log 2>&1
+"$VK_PATH/virtual-kubelet" --nodename $NODENAME --provider mock --provider-config $HOME/.host-cfg.json --klog.v 3 > $HOME/$NODENAME.log 2>&1
