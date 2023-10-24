cf5a7b6e0e043651170cb657e287b6bb4477c2dc
jeng-yuantsai <tsaie79@gmail.com>
2023-10-24 16:24:51 +0000

    recover the devcontainer.json, and add init.sh

---
+++ .devcontainer/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+kind create cluster
+cp ~/.kube/config /workspaces/vk-cmd/docker/images/base/activate/apiserver
+cd /workspaces/vk-cmd/docker/images/base
+make quick
+
+export NODENAME='vk-prom'
+sh /workspaces/vk-cmd/tools/pipeline/build_pipline_host.sh
