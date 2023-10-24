118673740ee9ae597af330bb615d9f3cdf383826
jeng-yuantsai <tsaie79@gmail.com>
2023-10-24 16:30:36 +0000

    update

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+kind delete cluster
 kind create cluster
 cp ~/.kube/config /workspaces/vk-cmd/docker/images/base/activate/apiserver
 cd /workspaces/vk-cmd/docker/images/base
 make quick
 
 export NODENAME='vk-prom'
-sh /workspaces/vk-cmd/tools/pipeline/build_pipline_host.sh
+sh /workspaces/vk-cmd/tools/pipeline/build_pipline_host.sh &
