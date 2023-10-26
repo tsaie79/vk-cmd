72e3e9e785e0d778d9d19215555ce640138540fa
tsaie79 <tsaie79@gmail.com>
2023-10-26 16:20:15 +0000

    This is uploaded as jlabtsai/random:v20231026

--- prom/test-app/random/gen-random.sh
+++ prom/test-app/random/gen-random.sh
@@ -4,6 +4,22 @@
 echo "Generating random numbers"
 
 # function 1: generate a random number and sleep for 100s
-while true; do
-    sleep 3
-done
+function gen_random1() {
+    while true
+    do
+        echo $((RANDOM%100))
+        sleep 100
+    done
+}
+
+# function 2: generate a random number and sleep for 200s
+function gen_random2() {
+    while true
+    do
+        echo $((RANDOM%100))
+        sleep 200
+    done
+}
+
+
+gen_random1 & gen_random2
