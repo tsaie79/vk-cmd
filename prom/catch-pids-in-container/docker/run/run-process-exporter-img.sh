f2cf22671de8eeca716ad07b7ec7b0213a83adbc
tsaie79 <tsaie79@gmail.com>
2023-10-29 05:42:58 +0000

    test auto exporter

--- prom/catch-pids-in-container/docker/run/run-process-exporter-img.sh
+++ prom/catch-pids-in-container/docker/run/run-process-exporter-img.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 
-export PROCESS_EXPORTER_IMG="jlabtsai/process-exporter:v0.1"
-export PROCESS_EXPORTER_PORT="1111"
+export PROCESS_EXPORTER_IMG="jlabtsai/process-exporter:v0.2"
+export PROCESS_EXPORTER_PORT="1911"
 
 
-docker run -d --rm -e PROCESS_EXPORTER_PORT=$PROCESS_EXPORTER_PORT -p $PROCESS_EXPORTER_PORT:$PROCESS_EXPORTER_PORT -v /proc:/host_proc $PROCESS_EXPORTER_IMG
+# docker run -d --rm -e PROCESS_EXPORTER_PORT=$PROCESS_EXPORTER_PORT -p $PROCESS_EXPORTER_PORT:$PROCESS_EXPORTER_PORT -v /proc:/host_proc $PROCESS_EXPORTER_IMG
 
 
-# shifter --image=$PROCESS_EXPORTER_IMG -V /proc:/host_proc --entrypoint
+ssh -NfR $PROCESS_EXPORTER_PORT:localhost:$PROCESS_EXPORTER_PORT mylin
+shifter --image=$PROCESS_EXPORTER_IMG -V /proc:/host_proc --entrypoint
