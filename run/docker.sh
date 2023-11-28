51daae657b14adb8302b509928c8cab899f8d9b7
tsaie79 <tsaie79@gmail.com>
2023-11-28 12:46:58 -0500

    Update NODENAME and vk-cmd version

--- run/docker.sh
+++ run/docker.sh
@@ -7,9 +7,9 @@ ssh -NfL 42053:localhost:42053 tsai@jiriaf2301.jlab.org
 sh ../tools/pipeline/build_pipline_host.sh &
 
 ## name node
-export NODENAME="vk-lin"
+export NODENAME="vk"
 export HOST_KUBECONFIG="$HOME/.kube"
 export KUBECONFIG="/.kube/config"
 
 ## run vk-cmd
-docker run -d -v $HOME/hostpipe:/root/hostpipe -v $HOST_KUBECONFIG:/.kube -e KUBECONFIG=/.kube/config -e NODENAME=$NODENAME --network="host" jlabtsai/vk-cmd:latest
+docker run -d -v $HOME/hostpipe:/root/hostpipe -v $HOST_KUBECONFIG:/.kube -e KUBECONFIG=/.kube/config -e NODENAME=$NODENAME --network="host" jlabtsai/vk-cmd:v20231122
