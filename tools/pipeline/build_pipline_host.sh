63745c9983be7c3a3277ac7ddc79b0531fc46d60
tsaie79 <tsaie79@gmail.com>
2023-12-04 00:38:53 -0500

    Refactor build_pipline_host.sh script to execute commands from vk-cmd using a subshell

--- tools/pipeline/build_pipline_host.sh
+++ tools/pipeline/build_pipline_host.sh
@@ -13,9 +13,21 @@ if [ ! -p "$HOME/hostpipe/vk-cmd" ]; then
 fi
 
 # while true do eval "$(cat $HOME/hostpipe/vk-cmd)" save stderror and stdout to diferent files
-while true; do
-    eval "$(cat $HOME/hostpipe/vk-cmd)" > $HOME/hostpipe/pipeline.out 2> $HOME/hostpipe/pipeline.err
-done
+# while true; do
+#     eval "$(cat $HOME/hostpipe/vk-cmd)" > $HOME/hostpipe/pipeline.out 2> $HOME/hostpipe/pipeline.err
+# done
 
 # maybe we cat revise it to:
 ## while true; do bash -s < fifo 1> stdout 2> stderr; done
+
+
+while true; do
+    # Reading the command from vk-cmd, executing it within a subshell, and capturing its PID
+    { cat "$HOME/hostpipe/vk-cmd" & echo $! >&3; } 3> $HOME/hostpipe/pid_file | bash -s > $HOME/hostpipe/pipeline.out 2> $HOME/hostpipe/pipeline.err
+    read pid < $HOME/hostpipe/pid_file
+
+    echo "PID of the command written to vk-cmd: $pid"
+
+    # Other operations using $pid
+    wait $pid  # Wait for this process to finish if necessary
+done
