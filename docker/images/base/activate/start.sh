b240b61fc23b11b7cac29083cae2fbbfdc31de97
tsaie79 <tsaie79@gmail.com>
2023-12-05 19:58:52 -0500

    Update start.sh script with new environment variables

--- docker/images/base/activate/start.sh
+++ docker/images/base/activate/start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# export KUBECONFIG="$VK_PATH/config"
 export VKUBELET_POD_IP="10.250.64.71"
 export APISERVER_CERT_LOCATION="client.crt"
 export APISERVER_KEY_LOCATION="client.key"
 export KUBELET_PORT="10250"
 
+export KUBECONFIG="config"
+export NODENAME="vk-cmd"
+
 echo "{\"$NODENAME\": {\"cpu\": \"0\", \"memory\": \"0Gi\", \"pods\": \"0\"}}" > .host-cfg.json
 
-"virtual-kubelet" --nodename $NODENAME --provider mock --provider-config .host-cfg.json --klog.v 3 > $NODENAME.log 2>&1
+./virtual-kubelet --nodename $NODENAME --provider mock --provider-config .host-cfg.json --klog.v 3 > $NODENAME.log 2>&1
