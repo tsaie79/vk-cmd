254e86dff96d058609a651cd965679501b45df9f
tsaie79 <tsaie79@gmail.com>
2023-12-07 14:16:39 -0500

    Update start.sh script with kubeconfig and nodename variables

--- docker/images/base/activate/start.sh
+++ docker/images/base/activate/start.sh
@@ -5,9 +5,9 @@ export APISERVER_CERT_LOCATION="client.crt"
 export APISERVER_KEY_LOCATION="client.key"
 export KUBELET_PORT="10250"
 
-export KUBECONFIG="config"
-export NODENAME="vk-cmd"
+# export KUBECONFIG="config"
+# export NODENAME="vk-cmd"
 
-echo "{\"$NODENAME\": {\"cpu\": \"0\", \"memory\": \"0Gi\", \"pods\": \"0\"}}" > .host-cfg.json
+echo "{\"$2\": {\"cpu\": \"0\", \"memory\": \"0Gi\", \"pods\": \"0\"}}" > .host-cfg.json
 
-./virtual-kubelet --nodename $NODENAME --provider mock --provider-config .host-cfg.json --klog.v 3 > $NODENAME.log 2>&1
+./virtual-kubelet --kubeconfig $1 --nodename $2 --provider mock --provider-config .host-cfg.json --klog.v 3 > $2.log 2>&1
