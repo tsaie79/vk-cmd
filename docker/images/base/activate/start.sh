e92bc44c697f0f603fc543e0a4b7c0e89cff2c2e
tsaie79 <tsaie79@gmail.com>
2023-12-11 00:06:41 -0500

    Remove commented out code and enable virtual-kubelet in start.sh

--- docker/images/base/activate/start.sh
+++ docker/images/base/activate/start.sh
@@ -8,6 +8,7 @@ export KUBELET_PORT="10250"
 # export KUBECONFIG="config"
 # export NODENAME="vk-cmd"
 
-echo "{\"$2\": {\"cpu\": \"0\", \"memory\": \"0Gi\", \"pods\": \"0\"}}" > .host-cfg.json
+# echo "{\"$2\": {\"cpu\": \"0\", \"memory\": \"0Gi\", \"pods\": \"0\"}}" > .host-cfg.json
 
-./virtual-kubelet --kubeconfig $1 --nodename $2 --provider mock --provider-config .host-cfg.json --klog.v 3 > $2.log 2>&1
+# ./virtual-kubelet --kubeconfig $1 --nodename $2 --provider mock --provider-config .host-cfg.json --klog.v 3 > $2.log 2>&1
+./virtual-kubelet --kubeconfig $1 --nodename $2 --provider mock --klog.v 3 > $2.log 2>&1
