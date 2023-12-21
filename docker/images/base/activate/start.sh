a3d85574b51a28699dc8921196d395ea608a40d8
tsaie79 <tsaie79@gmail.com>
2023-12-21 13:33:44 -0500

    Update start.sh script to use variables for KUBECONFIG and NODENAME

--- docker/images/base/activate/start.sh
+++ docker/images/base/activate/start.sh
@@ -5,10 +5,10 @@ export APISERVER_CERT_LOCATION="client.crt"
 export APISERVER_KEY_LOCATION="client.key"
 export KUBELET_PORT=$4 #"10250"
 
-# export KUBECONFIG="config"
-# export NODENAME="vk-cmd"
+export KUBECONFIG=$1
+export NODENAME=$2
 
 # echo "{\"$2\": {\"cpu\": \"0\", \"memory\": \"0Gi\", \"pods\": \"0\"}}" > .host-cfg.json
 
 # ./virtual-kubelet --kubeconfig $1 --nodename $2 --provider mock --provider-config .host-cfg.json --klog.v 3 > $2.log 2>&1
-./virtual-kubelet --kubeconfig $1 --nodename $2 --provider mock --klog.v 3 > $2.log 2>&1
+./virtual-kubelet --kubeconfig $KUBECONFIG --nodename $NODENAME --provider mock --klog.v 3 > $2.log 2>&1
