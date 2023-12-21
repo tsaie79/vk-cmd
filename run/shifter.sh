6f4d5ee59bcda0ffde63dee0ddfdba93b0e5f6b8
tsaie79 <tsaie79@gmail.com>
2023-12-21 10:25:02 -0500

    Update port forwarding in shifter.sh script

--- run/shifter.sh
+++ run/shifter.sh
@@ -6,7 +6,7 @@ export NODENAME="vk-direct"
 export KUBECONFIG="/global/homes/j/jlabtsai/run-vk/kubeconfig/mylin"
 
 
-ssh -NfL 33469:localhost:33469 mylin
+ssh -NfL 44875:localhost:44875 mylin
 
 shifter --image=docker:jlabtsai/vk-cmd:no-vk-container -- /bin/bash -c "cp -r /vk-cmd `pwd`"
 
