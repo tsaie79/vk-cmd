e58dce9f3be6150cfc36418f10bdb88eb82a3241
tsaie79 <tsaie79@gmail.com>
2023-12-21 13:34:00 -0500

    Update shifter.sh script to add VKUBELET_POD_IP and KUBELET_PORT variables

--- run/shifter.sh
+++ run/shifter.sh
@@ -4,12 +4,15 @@
 
 export NODENAME="vk-direct"
 export KUBECONFIG="/global/homes/j/jlabtsai/run-vk/kubeconfig/mylin"
+export VKUBELET_POD_IP="127.0.0.1"
+export KUBELET_PORT="10250"
 
 
 ssh -NfL 44875:localhost:44875 mylin
+ssh -NfR $KUBELET_PORT:localhost:$KUBELET_PORT mylin
 
-shifter --image=docker:jlabtsai/vk-cmd:no-vk-container -- /bin/bash -c "cp -r /vk-cmd `pwd`"
+shifter --image=docker:jlabtsai/vk-cmd:add-control -- /bin/bash -c "cp -r /vk-cmd `pwd`"
 
 cd `pwd`/vk-cmd
 
-./start.sh $KUBECONFIG $NODENAME
+./start.sh $KUBECONFIG $NODENAME $VKUBELET_POD_IP $KUBELET_PORT
