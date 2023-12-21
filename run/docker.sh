8cac07741f6a20374d0c5d41caa1f9492afcae36
tsaie79 <tsaie79@gmail.com>
2023-12-21 13:33:54 -0500

    Update docker.sh script to include VKUBELET_POD_IP and KUBELET_PORT variables

--- run/docker.sh
+++ run/docker.sh
@@ -5,14 +5,17 @@
 ## name node
 export NODENAME="vk-direct-lin"
 export KUBECONFIG="/home/jeng-yuantsai/.kube/config"
-
+export VKUBELET_POD_IP="127.0.0.1"
+export KUBELET_PORT="10250"
 
 ## run vk-cmd
-container_id=$(docker run -itd --rm jlabtsai/vk-cmd:no-vk-container)
+container_id=$(docker run -itd --rm jlabtsai/vk-cmd:add-control)
 docker cp ${container_id}:/vk-cmd `pwd` && docker stop ${container_id}
 
 cd `pwd`/vk-cmd
 
-./start.sh "$KUBECONFIG" "$NODENAME"&
 
 echo "api-server config: $KUBECONFIG; nodename: $NODENAME is runnning..."
+echo "vk api-server: $VKUBELET_POD_IP; vk kubelet port: $KUBELET_PORT"
+
+./start.sh $KUBECONFIG $NODENAME $VKUBELET_POD_IP $KUBELET_PORT
