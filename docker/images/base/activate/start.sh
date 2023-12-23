eb1483cd9bfeb73381ccbe0e7c76ca66b01497db
tsaie79 <tsaie79@gmail.com>
2023-12-22 22:29:13 -0500

    Remove commented out code in start.sh

--- docker/images/base/activate/start.sh
+++ docker/images/base/activate/start.sh
@@ -8,7 +8,4 @@ export KUBELET_PORT=$4 #"10250"
 export KUBECONFIG=$1
 export NODENAME=$2
 
-# echo "{\"$2\": {\"cpu\": \"0\", \"memory\": \"0Gi\", \"pods\": \"0\"}}" > .host-cfg.json
-
-# ./virtual-kubelet --kubeconfig $1 --nodename $2 --provider mock --provider-config .host-cfg.json --klog.v 3 > $2.log 2>&1
 ./virtual-kubelet --kubeconfig $KUBECONFIG --nodename $NODENAME --provider mock --klog.v 3 > $2.log 2>&1
