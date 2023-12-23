75e43c9b24b22ae9dbf46f9c4d97955005396a0e
tsaie79 <tsaie79@gmail.com>
2023-12-23 23:24:37 +0000

    Update shifter.sh script with new environment variables and SSH connections

--- run/shifter.sh
+++ run/shifter.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# if using swif2, cmd points to this file.
 
-export NODENAME="vk-direct"
-export KUBECONFIG="/global/homes/j/jlabtsai/run-vk/kubeconfig/mylin"
+export APISERVER="mylin2"
+export APISERVER_PORT="44877"
+export NODENAME="vk-nersc"
+export KUBECONFIG="/global/homes/j/jlabtsai/run-vk/kubeconfig/$APISERVER"
 export VKUBELET_POD_IP="127.0.0.1"
-export KUBELET_PORT="10250"
+export KUBELET_PORT="10257"
 
 
-ssh -NfL 44875:localhost:44875 mylin
-ssh -NfR $KUBELET_PORT:localhost:$KUBELET_PORT mylin
+#ssh -NfL 44875:localhost:44875 mylin
+ssh -NfL $APISERVER_PORT:localhost:$APISERVER_PORT $APISERVER
+ssh -NfR $KUBELET_PORT:localhost:$KUBELET_PORT $APISERVER
 
 shifter --image=docker:jlabtsai/vk-cmd:add-control -- /bin/bash -c "cp -r /vk-cmd `pwd`"
 
