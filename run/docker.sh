3ba2e296231407e9fbd0c96a99bbb73484c0a786
tsaie79 <tsaie79@gmail.com>
2024-01-08 22:14:08 +0000

    Update VKUBELET_POD_IP and KUBELET_PORT in docker.sh

--- run/docker.sh
+++ run/docker.sh
@@ -3,10 +3,10 @@
 ## ssh tunnel
 
 ## name node
-export NODENAME="vk"
+export NODENAME="vk-outside"
 export KUBECONFIG="$HOME/.kube/config"
-export VKUBELET_POD_IP="127.0.0.1"
-export KUBELET_PORT="10250"
+export VKUBELET_POD_IP="172.17.0.1"
+export KUBELET_PORT="10260"
 
 ## run vk-cmd
 container_id=$(docker run -itd --rm jlabtsai/vk-cmd:add-control)
@@ -16,6 +16,6 @@ cd `pwd`/vk-cmd
 
 
 echo "api-server config: $KUBECONFIG; nodename: $NODENAME is runnning..."
-echo "vk api-server: $VKUBELET_POD_IP; vk kubelet port: $KUBELET_PORT"
+echo "vk ip: $VKUBELET_POD_IP from view of metrics server; vk kubelet port: $KUBELET_PORT"
 
 ./start.sh $KUBECONFIG $NODENAME $VKUBELET_POD_IP $KUBELET_PORT
