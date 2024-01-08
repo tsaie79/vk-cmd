c7c0f826b86b130d1813193a1a8498299690f917
tsaie79 <tsaie79@gmail.com>
2024-01-08 22:14:18 +0000

    Update shifter.sh script with new environment variables

--- run/shifter.sh
+++ run/shifter.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 
-export APISERVER="mylin2"
-export APISERVER_PORT="44877"
+export CONTROL_PLANE_IP="mylin"
+export APISERVER_PORT="46859"
 export NODENAME="vk-nersc"
-export KUBECONFIG="/global/homes/j/jlabtsai/run-vk/kubeconfig/$APISERVER"
-export VKUBELET_POD_IP="127.0.0.1"
-export KUBELET_PORT="10257"
+export KUBECONFIG="/global/homes/j/jlabtsai/run-vk/kubeconfig/$CONTROL_PLANE_IP"
+export VKUBELET_POD_IP="172.17.0.1"
+export KUBELET_PORT="10261"
 
 
-#ssh -NfL 44875:localhost:44875 mylin
-ssh -NfL $APISERVER_PORT:localhost:$APISERVER_PORT $APISERVER
-ssh -NfR $KUBELET_PORT:localhost:$KUBELET_PORT $APISERVER
+ssh -NfL $APISERVER_PORT:localhost:$APISERVER_PORT $CONTROL_PLANE_IP
+ssh -NfR $KUBELET_PORT:localhost:$KUBELET_PORT $CONTROL_PLANE_IP # To make sure the port is open to all interfaces, one has to set GatewayPorts to yes in /etc/ssh/sshd_config and run sudo service ssh restart at mylin.
 
 shifter --image=docker:jlabtsai/vk-cmd:add-control -- /bin/bash -c "cp -r /vk-cmd `pwd`"
 
 cd `pwd`/vk-cmd
+
+echo "api-server config: $KUBECONFIG; nodename: $NODENAME is runnning..."
+echo "vk ip: $VKUBELET_POD_IP from view of metrics server; vk kubelet port: $KUBELET_PORT"
 
 ./start.sh $KUBECONFIG $NODENAME $VKUBELET_POD_IP $KUBELET_PORT
